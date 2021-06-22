c102b8431fedefccf7b7ee1a7a7faeecc1d8b3ac
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-23 11:47:00 +1200

    Added: mock data for discovery swipe screen testing

---
+++ lib/data/recipes.dart
@@ -0,0 +1,63 @@
+import 'package:fuelify/models/recipe.dart';
+
+final dummyRecipes = [
+  Recipe(
+      name: 'Lauren Turner',
+      designation: 'Content Writer',
+      userFavorites: 4,
+      bio:
+          'Psychology, science, and art are what helps me to learn the outside world and myself.',
+      age: 24,
+      imgUrl: 'assets/Recipe1.jpg',
+      location: 'North London'),
+  Recipe(
+    name: 'Lori Perez',
+    designation: 'UI Designer ',
+    userFavorites: 8,
+    bio:
+        'Travelling, adventures, extreme sports are also an integral part of me, but I like watching and admiring extreme sports rather than doing it ?',
+    location: 'Leeds',
+    age: 26,
+    imgUrl: 'assets/Recipe2.jpg',
+  ),
+  Recipe(
+    name: 'Christine Wallace',
+    designation: 'News Reporter',
+    userFavorites: 2,
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    location: 'Liverpool',
+    age: 23,
+    imgUrl: 'assets/Recipe3.jpg',
+  ),
+  Recipe(
+    name: 'Rachel Green',
+    designation: 'Architect',
+    userFavorites: 8,
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    location: 'Nottingham',
+    age: 22,
+    imgUrl: 'assets/Recipe4.jpg',
+  ),
+  Recipe(
+    name: 'Emma',
+    designation: 'Software Developer',
+    userFavorites: 3,
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    location: 'Manchester',
+    age: 25,
+    imgUrl: 'assets/Recipe5.jpg',
+  ),
+  Recipe(
+    name: 'Kim Wexler',
+    designation: 'Accountant',
+    userFavorites: 5,
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    location: 'Birmingham',
+    age: 30,
+    imgUrl: 'assets/Recipe6.jpg',
+  ),
+];
