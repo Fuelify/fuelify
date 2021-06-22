ed53aba31c94b773d7aebb64a70a3edc962d9ab8
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-23 11:48:55 +1200

    Created: a FeedbackPositionProvider change notifier that listens for draggable position changes

---
+++ lib/providers/feedback_position.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+enum SwipingDirection { left, right, none }
+
+class FeedbackPositionProvider extends ChangeNotifier {
+  double _dx = 0.0;
+  SwipingDirection _swipingDirection = SwipingDirection.none;
+
+  SwipingDirection get swipingDirection => _swipingDirection;
+
+  FeedbackPositionProvider() {
+    _swipingDirection = SwipingDirection.none;
+  }
+
+  void resetPosition() {
+    _dx = 0.0;
+    _swipingDirection = SwipingDirection.none;
+    notifyListeners();
+  }
+
+  void updatePosition(double changeInX) {
+    _dx = _dx + changeInX;
+    print('Drag dX: ' + _dx.toString());
+    if (_dx > 0) {
+      _swipingDirection = SwipingDirection.right;
+    } else if (_dx < 0) {
+      _swipingDirection = SwipingDirection.left;
+    } else {
+      _swipingDirection = SwipingDirection.none;
+    }
+    notifyListeners();
+  }
+}
