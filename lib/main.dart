cfdd489899aee213e57b1af4264ae43928a090f8
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-24 14:26:59 +1200

    Updated: code structure in main.dart to prevent provider reference error

--- lib/main.dart
+++ lib/main.dart
@@ -17,57 +17,59 @@ import 'package:provider/provider.dart';
 import 'models/user.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => DarkNotifier()),
+      ChangeNotifierProvider(create: (_) => FeedbackPositionProvider()),
+      ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ChangeNotifierProvider(create: (_) => UserProvider()),
+    ],
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of the mobile application
   @override
   Widget build(BuildContext context) {
     Future<User> getUserData() => UserPreferences().getUser();
 
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => DarkNotifier()),
-        ChangeNotifierProvider(create: (_) => FeedbackPositionProvider()),
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => UserProvider()),
-      ],
-      child: MaterialApp(
-          title: 'Fuelify App',
-          theme: ThemeData(
-            brightness: Provider.of<DarkNotifier>(context).isDark
-                ? Brightness.dark
-                : Brightness.light,
-            canvasColor: Theme.of(context).brightness == Brightness.dark ||
-                    Provider.of<DarkNotifier>(context).isDark
-                ? Colors.black45
-                : Colors.white,
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          home: FutureBuilder(
-              future: getUserData(),
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                  case ConnectionState.waiting:
-                    return CircularProgressIndicator();
-                  default:
-                    if (snapshot.hasError)
-                      return Text('Error: ${snapshot.error}');
-                    else if (!snapshot.hasData)
-                      return Login();
-                    else
-                      UserPreferences().removeUser();
-                    return Login();
-                }
-              }),
-          routes: {
-            '/dashboard': (context) => DashBoard(),
-            '/discovery': (context) => Discovery(),
-            '/login': (context) => Login(),
-            '/register': (context) => Register(),
+    return MaterialApp(
+      title: 'Fuelify App',
+      theme: ThemeData(
+        brightness: Provider.of<DarkNotifier>(context).isDark
+            ? Brightness.dark
+            : Brightness.light,
+        canvasColor: Theme.of(context).brightness == Brightness.dark ||
+                Provider.of<DarkNotifier>(context).isDark
+            ? Colors.black45
+            : Colors.white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FutureBuilder(
+          future: getUserData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return CircularProgressIndicator();
+              default:
+                if (snapshot.hasError)
+                  return Text('Error: ${snapshot.error}');
+                else if (!snapshot.hasData)
+                  return Login();
+                else
+                  UserPreferences().removeUser();
+                return Login();
+            }
           }),
+      routes: {
+        '/dashboard': (context) => DashBoard(),
+        '/discovery': (context) => Discovery(),
+        '/login': (context) => Login(),
+        '/register': (context) => Register(),
+      }
     );
   }
 }
