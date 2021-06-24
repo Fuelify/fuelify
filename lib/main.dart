c15cb006684f78c8d02754b86bda42a8742de925
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-24 14:21:10 +1200

    Added: dark notifier as a provider and implemented dark mode assertion in ThemeData field

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+
 import 'package:fuelify/screens/dashboard.dart';
 import 'package:fuelify/screens/discovery.dart';
 import 'package:fuelify/screens/login.dart';
 import 'package:fuelify/screens/register.dart';
 import 'package:fuelify/screens/welcome.dart';
+
 import 'package:fuelify/providers/auth.dart';
 import 'package:fuelify/providers/user_provider.dart';
 import 'package:fuelify/providers/feedback_position.dart';
+import 'package:fuelify/providers/dark_notifier.dart';
+
 import 'package:fuelify/dependencies/shared_preferences.dart';
 import 'package:provider/provider.dart';
 
@@ -23,6 +27,7 @@ class MyApp extends StatelessWidget {
 
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (_) => DarkNotifier()),
         ChangeNotifierProvider(create: (_) => FeedbackPositionProvider()),
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => UserProvider()),
@@ -30,6 +35,13 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
           title: 'Fuelify App',
           theme: ThemeData(
+            brightness: Provider.of<DarkNotifier>(context).isDark
+                ? Brightness.dark
+                : Brightness.light,
+            canvasColor: Theme.of(context).brightness == Brightness.dark ||
+                    Provider.of<DarkNotifier>(context).isDark
+                ? Colors.black45
+                : Colors.white,
             primarySwatch: Colors.blue,
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
