fa5900d72854862bea65b625cc43f563d29284eb
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-24 16:19:37 +1200

    Created: a networking provider for handling API data requests
    Added: a fetchRecipes call to API

---
+++ lib/providers/networking.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:fuelify/models/user.dart';
+import 'package:fuelify/dependencies/endpoints.dart';
+import 'package:fuelify/dependencies/user_preferences.dart';
+
+enum Status {
+  NotLoggedIn,
+  NotRegistered,
+  LoggedIn,
+  Registered,
+  Authenticating,
+  Registering,
+  LoggedOut
+}
+
+class NetworkProvider with ChangeNotifier {
+
+  /*
+    FETCH ADDITIONAL RECIPES
+  */
+
+  Future<Map<String, dynamic>> fetchRecipes() async {
+    var result;
+
+    notifyListeners();
+
+    var token = await UserPreferences().getToken(); // retrieves logged in users auth token
+    var url = Uri.parse(AppUrl.fetchRecipes);
+    
+    Response response = await get(
+      url,
+      headers: {'content-type': 'application/json', 'Authorization': token},
+    );
+    
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body) as Map<String, dynamic>;
+
+
+      notifyListeners();
+
+      result = responseData;
+    } else {
+      notifyListeners();
+      result = {'Message': 'Failed fetch additional recipes to showcase'};
+    }
+    return result;
+  }
+
+  static Future<Map<String, dynamic>> onValue(Response response) async {
+    var result;
+    final Map<String, dynamic> responseData = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      var userData = responseData['data'];
+
+      User authUser = User.fromJson(userData);
+
+      UserPreferences().saveUser(authUser);
+      result = {
+        'status': true,
+        'message': 'Successfully registered',
+        'data': authUser
+      };
+    } else {
+      result = {
+        'status': false,
+        'message': 'Registration failed',
+        'data': responseData
+      };
+    }
+
+    return result;
+  }
+
+  static onError(error) {
+    print("the error is $error.detail");
+    return {'status': false, 'message': 'Unsuccessful Request', 'data': error};
+  }
+}
