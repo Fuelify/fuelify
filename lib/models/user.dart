de9dec94b28e2c6745f1aafbda0503388139ff24
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-27 14:26:39 +1200

    Updated: user model to contain an image field for the users profile picture

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,15 +3,17 @@ class User {
   String name;
   String email;
   String phone;
+  String image;
   String type;
   String token;
   String refreshToken;
 
-  User({ 
-    this.userId = 0, 
-    this.name = "", 
+  User({
+    this.userId = 0,
+    this.name = "",
     this.email = "",
     this.phone = "",
+    this.image = "",
     this.type = "",
     this.token = "",
     this.refreshToken = "",
@@ -23,9 +25,9 @@ class User {
         name: responseData['name'] ?? "Joe Smith",
         email: responseData['email'],
         phone: responseData['phone'] ?? "315-988-5689",
+        image: responseData['image'] ?? "",
         type: responseData['type'],
         token: responseData['access_token'],
-        refreshToken: responseData['refresh_token']
-    );
+        refreshToken: responseData['refresh_token']);
   }
 }
