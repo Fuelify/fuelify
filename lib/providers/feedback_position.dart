bf242cce9232df84d7ba6dd2680c90e745dfc3f1
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-27 14:06:25 +1200

    Removed: card dragging printouts

--- lib/providers/feedback_position.dart
+++ lib/providers/feedback_position.dart
@@ -11,9 +11,11 @@ class FeedbackPositionProvider extends ChangeNotifier {
   double get dx => _dx;
   double get dy => _dy;
 
-  //FeedbackPositionProvider() {
-  //  _swipingDirection = SwipingDirection.none;
-  //}
+  FeedbackPositionProvider() {
+    _swipingDirection = SwipingDirection.none;
+    _dx = 0.0;
+    _dy = 0.0;
+  }
 
   void resetPosition() {
     _dx = 0.0;
@@ -25,16 +27,12 @@ class FeedbackPositionProvider extends ChangeNotifier {
   void updatePosition(double changeInX, double changeInY) {
     _dx = _dx + changeInX;
     _dy = _dy + changeInY;
-    print('Drag dX: ' + _dx.toString());
-    print('Drag dY: ' + _dy.toString());
     if (_dx.abs() > _dy.abs()) {
       // feedback in x direction dominates
       if (_dx > 0) {
         _swipingDirection = SwipingDirection.right;
-        print('Swiping Right');
       } else if (_dx < 0) {
         _swipingDirection = SwipingDirection.left;
-        print('Swiping Left');
       } else {
         _swipingDirection = SwipingDirection.none;
       }
@@ -42,10 +40,8 @@ class FeedbackPositionProvider extends ChangeNotifier {
       // feedback in y direction dominates
       if (_dy > 0) {
         _swipingDirection = SwipingDirection.down;
-        print('Swiping Down');
       } else if (_dy < 0) {
         _swipingDirection = SwipingDirection.up;
-        print('Swiping Up');
       } else {
         _swipingDirection = SwipingDirection.none;
       }
