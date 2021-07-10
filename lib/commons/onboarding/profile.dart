1837272565b4519c42c47ac7643afd7e210c36e0
Ethan Minier <ethan@fuelifyyourlife.com>
2021-07-10 19:27:10 +1200

    Implemented: custom drop down widget class for gender selection in profile form

--- lib/commons/onboarding/profile.dart
+++ lib/commons/onboarding/profile.dart
@@ -3,6 +3,14 @@ import 'package:intl/intl.dart';
 
 import 'package:fuelify/commons/widgets.dart';
 import 'package:fuelify/models/user.dart';
+import 'package:fuelify/commons/customdropdowns.dart';
+
+final genderOptions = [
+  DropDownOption(option: 'Male', icon: Icons.male),
+  DropDownOption(option: 'Female', icon: Icons.female),
+  DropDownOption(option: 'Prefer not to say', icon: Icons.not_interested),
+  DropDownOption(option: 'Prefer to self describe', icon: Icons.arrow_forward),
+];
 
 class ProfileInfoWidget extends StatelessWidget {
   final User user;
@@ -44,43 +52,70 @@ class ProfileInfoWidget extends StatelessWidget {
       readOnly: true,
       controller: _dateController,
       decoration: buildInputDecoration("Location", Icons.calendar_today),
+      onSaved: (value) => _birthday = value,
       onTap: () async {
         await showDatePicker(
-                context: context,
-                initialDate: _selectedDate,
-                firstDate: DateTime.utc(1900, 1, 1),
-                lastDate: DateTime.now(),
-                initialDatePickerMode: DatePickerMode.year,
-                helpText: 'Select Date of Birth', // Title
-                errorFormatText: 'Enter valid date',
-                fieldLabelText: 'Enter date',
-                fieldHintText: 'Month/Date/Year',
-          ).then((selectedDate) {
-            if (selectedDate != null) {
-              _selectedDate = selectedDate;
-              _dateController.text =
-                  DateFormat('yyyy-MM-dd').format(selectedDate);
-            }
-          });
-        },
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Please enter date.';
+          context: context,
+          initialDate: _selectedDate,
+          firstDate: DateTime.utc(1900, 1, 1),
+          lastDate: DateTime.now(),
+          initialDatePickerMode: DatePickerMode.year,
+          helpText: 'Select Date of Birth', // Title
+          errorFormatText: 'Enter valid date',
+          fieldLabelText: 'Enter date',
+          fieldHintText: 'Month/Date/Year',
+        ).then((selectedDate) {
+          if (selectedDate != null) {
+            _selectedDate = selectedDate;
+            _dateController.text =
+                DateFormat('yyyy-MM-dd').format(selectedDate);
           }
-          return null;
+        });
+      },
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter date.';
+        }
+        return null;
       },
     );
 
-    /*final birthdayField = TextFormField(
-      autofocus: false,
-      onSaved: (value) => _birthday = value,
-      decoration: buildInputDecoration("Location", Icons.calendar_today),
-    );*/
-
-    final genderField = TextFormField(
+    /*final genderField = TextFormField(
       autofocus: false,
       onSaved: (value) => _gender = value,
       decoration: buildInputDecoration("Location", Icons.male),
+    );*/
+    final genderField = CustomDropdown<int>(
+      child: Text(
+        "",
+      ),
+      onChange: (int value, int index) => print(genderOptions[index].option),
+      onSaved: (int value, int index) => _gender = genderOptions[index].option,
+      dropdownButtonStyle: DropdownButtonStyle(
+        mainAxisAlignment: MainAxisAlignment.start,
+        //width: 170,
+        height: 40,
+        elevation: 1,
+        backgroundColor: Colors.white,
+        primaryColor: Colors.black87,
+      ),
+      dropdownStyle: DropdownStyle(
+        borderRadius: BorderRadius.circular(8),
+        elevation: 6,
+        padding: EdgeInsets.all(5),
+      ),
+      items: genderOptions
+          .asMap()
+          .entries
+          .map(
+            (item) => DropDownItem<int>(
+              value: item.key + 1,
+              text: item.value.option,
+              iconData: item.value.icon,
+              isSelected: false,
+            ),
+          )
+          .toList(),
     );
 
     /*
