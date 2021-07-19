4416d5a9b4884fc7a3a8ca002ca8560a4bd4d308
Ethan Minier <ethan@fuelifyyourlife.com>
2021-07-19 13:39:43 +1200

    Created: selectedColor and unselectedColor constants as well as unit options list var in constants.dart file

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,26 @@ import 'package:flutter/material.dart';
 const kBackgroundColor = Color(0xFF202020);
 const kPrimaryColor = Color(0xFFFFBD73);
 const primaryThemeColor = Colors.grey;
+
+/*
+  Onboarding Question Options
+*/
+
+var selectedColor = Colors.teal.shade300;
+const unselectedColor = Colors.white;
+
+var UnitOptions = [
+  "Imperial",
+  "Metric"
+];
+
+int getIndexOfOptionList(String selectedOption) {
+  int optionIndex = 0;
+  for (String text in UnitOptions) {
+    if (text == selectedOption) {
+      return optionIndex;
+    }
+    optionIndex++;
+  }
+  return optionIndex;
+}
