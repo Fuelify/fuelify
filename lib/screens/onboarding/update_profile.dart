88f31fb53025099ceaf1d6ff87f9056e0ff4f6b1
Ethan Minier <ethan@fuelifyyourlife.com>
2021-07-28 15:40:50 +1200

    Implemented: revised stack and overlayed continue button design

--- lib/screens/onboarding/update_profile.dart
+++ lib/screens/onboarding/update_profile.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:fuelify/dependencies/user_preferences.dart';
 import 'package:fuelify/models/user.dart';
 import 'package:fuelify/providers/user.dart';
 
+import 'package:fuelify/commons/buttons.dart';
 import 'package:fuelify/commons/profile/photo.dart';
 import 'package:fuelify/commons/onboarding/profile.dart';
 
@@ -18,27 +20,72 @@ class _ProfileUpdateState extends State<ProfileUpdate> {
   Widget build(BuildContext context) {
     User user = Provider.of<UserProvider>(context).user;
 
+    final formKey = new GlobalKey<FormState>();
+
+    Map<String, dynamic> profileData =
+        {}; // initialize empty personal data map
+
+    final nextView = (routeName) {
+      print(routeName);
+      Navigator.pushNamed(context, routeName);
+    };
+
+    var recordData = () {
+      final form = formKey.currentState;
+
+      if (form!.validate()) {
+        form.save();
+        print(profileData);
+        UserProfile().saveProfileData(profileData);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content:
+                Text("Invalid Form: " + "Please Complete the form properly")));
+      }
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Profile Information"),
         elevation: 0.1,
       ),
-      body: ListView(
+      body: Stack(
+        fit: StackFit.expand,
         children: [
-          ProfilePhotoWidget(
-            user: user,
-            onClicked: () async {},
+          Column(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 60.0),
+                  child: ListView(
+                    children: [
+                      ProfilePhotoWidget(
+                        user: user,
+                        onClicked: () async {},
+                      ),
+                      ProfileInfoWidget(
+                        user: user,
+                        formKey: formKey,
+                        profileData: profileData,
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-          ProfileInfoWidget(
-            user: user,
-            buttonText: "Continue",
-            buttonRoute: "/onboarding/diet"
+          FullWidthOverlayButtonWidget(
+            text: 'Continue',
+            onClicked: () {
+              recordData();
+              nextView("/onboarding/personal-details");
+            },
           ),
-          SizedBox(
-            height: 24,
-          ),
-        ],
-      ),
+        ]
+      )
     );
   }
 }
