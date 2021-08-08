aaae8fd5e06c4c947b8bf5ea9699e1c35d8fbc01
Ethan Minier <ethan@fuelifyyourlife.com>
2021-08-08 14:59:48 +1200

    Added: a user/profile/update endpoint to endpoint url class

--- lib/dependencies/endpoints.dart
+++ lib/dependencies/endpoints.dart
@@ -1,12 +1,18 @@
 // API Endpoints
 class AppUrl {
   static const String liveBaseURL = "http://18.117.207.252"; //"https://user.fuelifyyourlife.com";
-  static const String localBaseURL = "http://localhost:3000";
+  static const String localBaseURL = "http://192.168.1.79:3000";
 
-  static const String base = liveBaseURL; //localBaseURL
+  static const String base = localBaseURL; //liveBaseURL
   static const String login = base + "/user/login";
   static const String register = base + "/user/register";
   static const String forgotPassword = base + "/forgot-password";
   static const String testToken = base + "/tokentest";
+
+  /*
+    User Endpoints
+  */
+  static const String userUpdateProfile = base + "/user/profile/update";
+
   static const String fetchFoods = base + "/foods/fetch?limit=10";
 }
