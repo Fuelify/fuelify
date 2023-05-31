d632c4eb6e97a6e6abc41336fb11d7c1edbeb383
Ethan Minier <ethan@fuelifyyourlife.com>
2023-05-31 19:47:20 -0400

    Created: meal data model class

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,32 +1,35 @@
+import 'package:fuelify/models/recipe.dart';
+
 class Meal {
   final String id;
-  final String title;
-  final String description;
-  /*final String imageUrl;
-  final List<String> ingredients;
-  final List<String> steps;
-  final DateTime date;*/
+  final DateTime date;
+  final Recipe recipe;
   
   Meal({
     required this.id,
-    required this.title,
-    required this.description,
-    //required this.imageUrl,
-    //required this.ingredients,
-    //required this.steps,
-    //required this.date,
+    required this.date,
+    required this.recipe,
   });
 
   // Factory method to create a Meal from a Map
   factory Meal.fromJSON(Map<String, dynamic> map) {
     return Meal(
       id: map['id'],
-      title: map['title'],
-      description: map['description'],
-      /*imageUrl: map['imageUrl'],
-      ingredients: List<String>.from(map['ingredients']),
-      steps: List<String>.from(map['steps']),
-      date: DateTime.parse(map['date']),*/
+      date: DateTime.parse(map['date']),
+      recipe: Recipe(
+        title: map['recipe'] != null ? map['recipe']['title'] : 'Recipe ${map['id']}', 
+        imageUrl: map['recipe'] != null ? map['recipe']['imageUrl'] : 'https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=1480&t=st=1685316887~exp=1685317487~hmac=c0ba33339237cf2ec3012137693a7aa1f2b3b217c235530355ff7b979dacddc8',
+        description: map['recipe'] != null ? map['recipe']['title'] : 'Description of recipe',
+        ingredients: map['recipe'] != null ? map['recipe']['title'] : [],
+        instructions: map['recipe'] != null ? map['recipe']['title'] : [],
+        prepTime: map['recipe'] != null ? map['recipe']['prepTime'] : null,
+        cookTime: map['recipe'] != null ? map['recipe']['cookTime'] : null,
+        nutritionalStats: NutritionalStats(
+          fat: map['recipe'] != null ? map['recipe']['nutritionalStats']['fat'] : 'N/A', 
+          protein: map['recipe'] != null ? map['recipe']['nutritionalStats']['protein'] : 'N/A',
+          carbohydrates: map['recipe'] != null ? map['recipe']['nutritionalStats']['carbohydrates'] : 'N/A', 
+        )
+      )
     );
   }
 
@@ -34,12 +37,6 @@ class Meal {
   Map<String, dynamic> toJSON() {
     return {
       'id': id,
-      'title': title,
-      'description': description,
-      /*'imageUrl': imageUrl,
-      'ingredients': ingredients,
-      'steps': steps,
-      'date': date.toIso8601String(),*/
     };
   }
 }
